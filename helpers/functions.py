5eac7688d54bb0445c0f47a977328c718faa97e4
NiranjanHonnungar <84075920+NiranjanHonnungar@users.noreply.github.com>
2022-12-04 15:23:26 +0530

    payback period endpoint

--- helpers/functions.py
+++ helpers/functions.py
@@ -5,3 +5,7 @@ def simple_interest_rate(amount_paid:float, principle_amount:float, months:int):
     rate = (interest_paid*100)/(principle_amount*term)
     return rate
 
+def payback_period(years_before_recovery:int, unrecovered_cost:float, cash_flow:float):
+    period=years_before_recovery+(unrecovered_cost/cash_flow)
+    return period
+
