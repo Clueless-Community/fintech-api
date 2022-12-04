c5053df2569b480470e26c9b4edb90174f2aa482
gizmo50 <usanyuktha@gmail.com>
2022-12-05 00:13:35 +0530

    Issue 14 Resolved

--- helpers/functions.py
+++ helpers/functions.py
@@ -4,4 +4,9 @@ def simple_interest_rate(amount_paid:float, principle_amount:float, months:int):
     interest_paid = amount_paid-principle_amount
     rate = (interest_paid*100)/(principle_amount*term)
     return rate
+def future_sip(interval_investment:float, rate_of_return:float, number_of_payments:int):
+    interest= (rate_of_return/100)/12
+    value = interval_investment*((1+ interest)**number_of_payments - 1)*(1+interest)/interest
+    return value
+
 
--- main.py
+++ main.py
@@ -56,3 +56,22 @@ def simple_interest_rate(amount_paid: float, principle_amount: float, months: in
         }
     except:
         return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@app.get(
+    "/future_sip",
+    tags=["future_sip"],
+    description="Calculate Future Value of SIP",
+)
+def future_sip(interval_investment:float, rate_of_return:float, number_of_payments:int):
+    try:
+        value = functions.future_sip(interval_investment, rate_of_return, number_of_payments)
+        return {
+            "Tag": "Future Value of SIP",
+            "Investment at every Interval": interval_investment,
+            "Interest": (rate_of_return/100)/12,
+            "Number of Payments": number_of_payments,
+            "Future Value": f"{value}%",
+        }
+    except:
+        return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
