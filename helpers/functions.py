c715c1474bd164c8290015cb878c82b6da1d45eb
Lakshya <lakshya30onkar@gmail.com>
2022-12-05 20:12:50 +0530

    CAGR function added

--- helpers/functions.py
+++ helpers/functions.py
@@ -9,3 +9,9 @@ def simple_interest_rate(amount_paid:float, principle_amount:float, months:int):
 def return_on_investment(gain_from_investment:float, cost_of_investment:float):
     roi = (gain_from_investment - cost_of_investment)/cost_of_investment
     return roi
+
+# Function to calculate Compounded Annual Growth Rate (CAGR)
+def compounded_annual_growth_rate(end_investment_value:float, initial_investment_value:float, years:int):
+    n=1/years
+    cagr=(end_investment_value/initial_investment_value)**n -1
+    return cagr
