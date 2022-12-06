f5ffb543bd6c6759d2d71f3c8ce6bca230ad18ce
Pundarikaksha <98964611+pundarikaksha7@users.noreply.github.com>
2022-12-06 09:39:41 +0530

    Added Jensen's Alpha endpoint

--- main.py
+++ main.py
@@ -196,4 +196,24 @@ def compounded_annual_growth_rate(end_investment_value:float, initial_investment
         }
     except:
         return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            
+#Endpoint to calculate Jensen's Alpha
+@app.get(
+    "/jensens_alpha",
+    tags=["jensens_alpha"],
+    description="Calculate Jensen's Alpha of a market return",
+)
+def jensens_alpha(return_from_investment:float,return_of_appropriate_market_index:float,risk_free_rate:float,beta:float):
+    try:
+        alpha = functions.jensens_alpha(return_from_investment,return_of_appropriate_market_index,risk_free_rate,beta)
+        return {
+            "Tag": "Jensen's Alpha",
+            "Total return from investment": return_from_investment,
+            "Return of appropriate market index": return_of_appropriate_market_index,
+            "Risk free rate": risk_free_rate,
+            "Beta of the portfolio investment w.r.t chosen market index":beta,
+            "Alpha of the return ":f'{alpha}%'
+        }
+    except:
+        return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)            
 
