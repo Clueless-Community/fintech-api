6c576d236da3da342e0dedb6197bba76a400c8f9
chandansgowda <chandansuresh007@gmail.com>
2022-12-10 14:12:10 +0530

    added inventory shrinkage rate endpoint

--- helpers/functions.py
+++ helpers/functions.py
@@ -4,13 +4,13 @@ def simple_interest_rate(amount_paid:float, principle_amount:float, months:int):
     interest_paid = amount_paid-principle_amount
     rate = (interest_paid*100)/(principle_amount*term)
     return rate
-    
+
 # Function to Calculate Loan Emi
 def loan_emi(principle_amount: float, annual_rate: float, months: int):
     monthly_rate=annual_rate/1200
     emi= (principle_amount*monthly_rate*(1+monthly_rate)**months)/(((1+monthly_rate)**months -1))
     return emi
-    
+
 def future_sip(interval_investment:float, rate_of_return:float, number_of_payments:int):
     interest= (rate_of_return/100)/12
     value = interval_investment*((1+ interest)**number_of_payments - 1)*(1+interest)/interest
@@ -21,7 +21,7 @@ def payback_period(years_before_recovery:int, unrecovered_cost:float, cash_flow:
     period=years_before_recovery+(unrecovered_cost/cash_flow)
     return period
 
-# Function to Calculate Compound Intrest 
+# Function to Calculate Compound Intrest
 def compound_interest(principal_amount:float, intrest_rate:float, years:int, compounding_period:int):
     amount = principal_amount * (pow((1 + (intrest_rate / compounding_period)), (compounding_period*years)))
     print(amount)
@@ -57,14 +57,14 @@ def compounded_annual_growth_rate(end_investment_value:float, initial_investment
 #Function to calculate Jensens Alpha
 def jensens_alpha(return_from_investment:float,return_of_appropriate_market_index:float,risk_free_rate:float,beta:float):
     alpha=return_from_investment-(risk_free_rate+beta*(return_of_appropriate_market_index-risk_free_rate))
-    return alpha 
+    return alpha
 
-#Function to calculate Weighted Average Cost of Capital (WACC) 
+#Function to calculate Weighted Average Cost of Capital (WACC)
 def weighted_average_cost_of_capital(firm_equity:float,firm_debt:float,cost_of_equity:float,cost_of_debt:float,corporate_tax_rate:float):
     v=firm_debt+firm_equity
     wacc=(firm_equity*cost_of_equity/v)+(firm_debt*cost_of_debt*(1-corporate_tax_rate)/v)
     return wacc
-    
+
 # Function to calculate variance of a two asset portfolio
 def asset_portfolio(price_A:float, price_B:float, retrun1:float, return2:float, standard_dev_A:float, standard_dev_B:float, stock_correlation:float):
     weight_A = price_A/(price_A + price_B)
@@ -78,7 +78,7 @@ def put_call_parity(call_price:float, put_price:float, strike_price:float):
     future_price = call_price + strike_price - put_price
     return future_price
 
-#Function to calculate break even point 
+#Function to calculate break even point
 def break_even_point(fixed_cost:float, selling_price:float, variable_cost:float):
     contribution_margin = selling_price - variable_cost
     units = fixed_cost//contribution_margin
@@ -88,7 +88,7 @@ def break_even_point(fixed_cost:float, selling_price:float, variable_cost:float)
 #Function to calculate the Price-to-Earning ratio (P/E ratio):
 def price_to_earning_ratio(share_price:float, earnings_per_share:float):
     p_e_ratio = share_price//earnings_per_share
-    return p_e_ratio  
+    return p_e_ratio
 
 #Function to calculate the Dividend yield ratio:
 def dividend_yield_ratio(dividend_per_share:float,share_price:float):
@@ -96,9 +96,13 @@ def dividend_yield_ratio(dividend_per_share:float,share_price:float):
     return dividend_yield
 
 
-# Function to calculate the dividend payout ratio 
+# Function to calculate the dividend payout ratio
 def dividend_payout_ratio(dividend_per_share:float,earnings_per_share:float):
     dividend_payout = dividend_per_share//earnings_per_share
     return dividend_payout
-    
-     
+
+#Function to calculate Inventory Shrinkage
+def inventory_shrinkage_rate(recorded_inventory:float, actual_inventory:float):
+    inventory_shrinkage_rate = (recorded_inventory-actual_inventory)/recorded_inventory
+    return inventory_shrinkage_rate
+
