cf72afa44fecc5440951ae4e0b27d992137c7de1
Durgavamsi Krishna Pullapanthula <89008784+vamsikrishnarh7@users.noreply.github.com>
2022-12-11 22:14:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -587,3 +587,21 @@ def sharpe_ratio(portfolio_return: float, risk_free_rate: float, standard_deviat
         }
     except:
         return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+# Endpoint to calculate cost of goods sold
+@app.get(
+    "/cogs",
+    tags = ["cogs"],
+    description = "Calculate Cost of Goods Sold",
+)
+def cost_of_goods_sold(beginning_inventory:float,purchases:float,ending_inventory:float):
+    try:
+        cogs = functions.cost_of_goods_sold(beginning_inventory,purchases,ending_inventory)
+        return{
+            "Tag" = "Cost of Goods Sold",
+            "Beginning Inventory" : beginning_inventory,
+            "Purchases during the period" : purchases,
+            "Ending Inventory" : ending_inventory,
+            "Cost of Goods Sold(In Rupees)" : f"{cogs}",
+        }
+    except:
+        return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
