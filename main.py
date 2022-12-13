e1c13790805858f4cc3b06b420be988fbc737c3a
Durgavamsi Krishna Pullapanthula <89008784+vamsikrishnarh7@users.noreply.github.com>
2022-12-13 08:16:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -751,3 +751,45 @@ def acid_test_ratio(cash:float,marketable_securitie:float,accounts_receivable:fl
         }
     except:
         return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+#Endpoint to calculate inflation adjusted return 
+@app.get(
+    "/inflation-adjusted-return",
+    tag = ["inflation-adjusted-return"],
+    description = "Calculate Inflation Adjusted Return",
+)
+def inflation_adjusted_return(
+    beginning_price:float,
+    ending_price:float,
+    dividends:float,
+    beginning_cpi_level:float,
+    ending_cpi__level:float
+    ):
+    try:
+        stock_return = (ending_price-beginning_price+dividends) / beginning_price
+        inflation = (ending_cpi__level - beginning_cpi_level) / beginning_cpi_level
+        inflation_adj_return = functions.inflation_adjusted_return(beginning_price,ending_price,dividends,beginning_cpi_level,ending_cpi__level)
+        return{
+            "Tag" : "Inflation Adjusted Return",
+            "Stock Return" : f"{stock_return}%",
+            "Inflation Rate" : f"{inflation}%", 
+            "Inflation Adjusted Return" : f"{inflation_adj_return}%" ,
+        }
+    except:
+        return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+# Endpoint to calculate compound annual growth rate
+@app.get(
+    "/cogr",
+    tag = ["cogr"],   
+    description = "Calculate Compound Annual Growth Rate",
+)
+def compound_annual_growth_rate(beginning_value:float,ending_value:float,years:int):
+    try:
+        rate = functions.compound_annual_growth_rate(beginning_value,ending_value,years)
+        return{
+            "Tag" : "Coumpound Annual Growth Rate",
+            "Beginning Value" : beginning_value,
+            "Ending Value" : ending_value,
+            "Compound Annual Growth Rate" : f"{rate}%",
+        }
+    except:
+        return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
