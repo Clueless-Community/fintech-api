a7b42ffb48f2cdfa872719f41d3fef02a3d3c82d
chandansgowda <chandansuresh007@gmail.com>
2022-12-20 09:11:15 +0530

    added endpoint to calculate excess reserves

--- main.py
+++ main.py
@@ -1000,3 +1000,20 @@ def real_gdp(nominal_gdp:float, gdp_deflator:float):
     except:
         return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+@app.get(
+    "/excess_reserves",
+    tags=["excess_reserves"],
+    description="Excess Reserves",
+)
+def excess_reserves(deposits:float, reserve_requirement:float):
+    try:
+        excess_reserves = functions.excess_reserves(deposits, reserve_requirement)
+        return {
+            "Tag": "Excess Reserves",
+            "Deposits": deposits,
+            "Reserve Requirement": reserve_requirement,
+            "Excess Reserves": excess_reserves,
+        }
+    except:
+        return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
