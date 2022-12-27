6c02fcb1c743c74ee14c056d541121e3d75f1daa
chandansgowda <chandansuresh007@gmail.com>
2022-12-27 09:10:01 +0530

    added tax equivalent yield endpoint

--- main.py
+++ main.py
@@ -1287,3 +1287,22 @@ def retention_ratio(net_income:float,dividends: float):
         }
     except:
         return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+# Endpoint to calculate tax equivalent yield
+@app.get(
+    "/tax_equivalent_yield",
+    tags = ["tax_equivalent_yield"],
+    description = "Calculating tax equivalent yield",
+)
+def tax_equivalent_yield(tax_free_yield:float,tax_rate:float):
+    try:
+        tax_equivalent_yield = functions.tax_equivalent_yield(tax_free_yield,tax_rate)
+        return{
+            "Tag" : "Tax Equivalent Yield",
+            "Tax Free Yield" : tax_free_yield,
+            "Tax Rate" : tax_rate,
+            "Tax Equivalent Yield" : tax_equivalent_yield,
+        }
+    except:
+        return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
