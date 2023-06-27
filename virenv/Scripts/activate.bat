5eaf6336f3558262663619c467101c57234997c8
Suvamita Sadhukhan <103322378+suvamita@users.noreply.github.com>
2023-06-27 11:15:23 +0530

    Update activate.bat

--- virenv/Scripts/activate.bat
+++ virenv/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-
+   
 rem This file is UTF-8 encoded, so we need to update the current code page while executing it
 for /f "tokens=2 delims=:." %%a in ('"%SystemRoot%\System32\chcp.com"') do (
     set _OLD_CODEPAGE=%%a
