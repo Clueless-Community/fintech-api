c29a5712688030f820dc5c5e442d392d2090b543
Suvamita Sadhukhan <103322378+suvamita@users.noreply.github.com>
2023-07-23 02:42:46 +0530

    Update request_validators.py

--- validators/request_validators.py
+++ validators/request_validators.py
@@ -856,3 +856,22 @@ class calculateMarketCap(BaseModel):
 class calculateBvps(BaseModel):
     total_equity: float
     number_of_shares: float
+
+class net_income(BaseModel):
+    revenue: float
+    expenses: float
+
+class break_even_point(BaseModel):
+    fixed_costs: int 
+    sales_price_per_unit: float
+    variable_price_per_unit: float
+
+class day_sales_in_inventory_ratio(BaseModel):
+    avg_inventory : int
+    no_of_days: int
+    cost_of_goods_sold: int
+
+class cash_ratio(BaseModel):
+    cash :float
+    cash_equivalents: float
+    current_liabilities: float
